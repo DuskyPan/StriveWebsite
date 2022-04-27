19e72775904815a4bec32b6c4844770397444cba
DuskyPan <38621043+DuskyPan@users.noreply.github.com>
2022-04-27 23:02:04 +0100

    Delete characterQuiz.js

--- characterQuiz.js
+++
@@ -1,4 +0,0 @@
-const question = document.getElementById("question");
-const answers = Array.from(document.getElementsByClassName('answer-button'));
-
-const questionCount = document.getElementById('questionCount');
